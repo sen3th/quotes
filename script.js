f1859fb233193f6f0e91931b732fc85cda363b0b
Seneth <hi@seneth.me>
2025-11-15 21:09:00 -0500

    added favicon, added few quotes.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function newQuote() {
 }
 
 const quoteList = [
-    "Be yourself; everyone else is already taken. <br> <span class='author'> -Oscar Wilde </span>",
+    "Be yourself; everyone else is already taken. <span class='author'> <br> -Oscar Wilde </span>",
 
     "The only way to do great work is to love what you do. <br> <span class='author'>-Steve Jobs</span>",
 
@@ -15,7 +15,21 @@ const quoteList = [
 
     "Tot thine own self be true. <br> <span class='author'>-William Shakespeare</span>",
 
-    "You miss 100% of the shots you don't take. <span class='author'><br> -Wayne Gretzky</span>",
+    "You miss 100% of the shots you don't take. <span class='author'> <br> -Wayne Gretzky</span>",
     
-    "That which does not kill us makes us stronger. <span class='author'><br> -Friedrich Nietzsche</span>"
+    "That which does not kill us makes us stronger. <span class='author'> <br> -Friedrich Nietzsche</span>",
+
+    "Believe you can and you're halfway there. <span class='author'> <br> -Theodore Roosevelt</span>",
+
+    "There's no place like home. <span class='author'> <br> -Dorothy Gale</span>",
+
+    "The only thing we have to fear is fear itself. <span class='author'> <br> -Franklin D. Roosevelt</span>",
+
+    "So many books, so little time. <span class='author'> <br> -Frank Zappa</span>",
+
+    "You only live once, but if you do it right, once is enough. <span class='author'> <br> -Mae West</span>",
+
+    "Be the change that you wish to see in the world. <span class='author'> <br> -Mahatma Gandhi</span>",
+
+    "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel. <span class='author'> <br> -Maya Angelou</span>",
 ];
